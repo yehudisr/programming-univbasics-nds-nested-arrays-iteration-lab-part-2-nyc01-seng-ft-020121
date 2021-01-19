babee59a143f2908af1d59310549d294ef794258
yehudis raitman <yehudisr@gmail.com>
2021-01-19 00:11:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,7 +13,6 @@ def find_min_in_nested_arrays(src)
     while element_index < src[row_index].count do
     if src[row_index][element_index] < lowest_temp
       lowest_temp = src[row_index][element_index]
-     
      end 
   
    element_index += 1 
